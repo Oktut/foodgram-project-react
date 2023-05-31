3c70a37adc793d0de08098741730c0f91e24d81f
Oktut <113987664+Oktut@users.noreply.github.com>
2023-05-31 04:22:07 +0300

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -6,9 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 load_dotenv()
 
-SECRET_KEY = os.getenv(
-    'SECRET_KEY', default='django-insecure-*y@3h=^&d*!rmgt=^&$_&qervu)+hep-#gslpn86mzu_*ve+*p'
-)
+SECRET_KEY = 'django-insecure-*y@3h=^&d*!rmgt=^&$_&qervu)+hep-#gslpn86mzu_*ve+*p'
 
 DEBUG = os.getenv('DEBUG', default='False')
 
