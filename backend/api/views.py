98e9ccd1bc565a35c18e49f0348d11fa6d96f614
Oktut <ya.oktut@yandex.ru>
2023-06-03 16:32:18 +0300

    55

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.permissions import SAFE_METHODS, IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated, SAFE_METHODS
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
